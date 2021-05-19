fa57c0bf4f77dba8c3bcdaa5705a506b3bd6cd86
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-05-19 11:30:11 -0500

    M05-02 - Incomplete implementation. Runs.

--- WiredBrainCoffee.AdminApp/ViewModel/CoffeeVideoViewModel.cs
+++ WiredBrainCoffee.AdminApp/ViewModel/CoffeeVideoViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Azure.Storage.Blob;
+using WiredBrainCoffee.AdminApp.Service;
+using WiredBrainCoffee.Storage;
 
 namespace WiredBrainCoffee.AdminApp.ViewModel
 {
@@ -6,12 +8,30 @@ namespace WiredBrainCoffee.AdminApp.ViewModel
     {
 
         private CloudBlockBlob cloudBlockBlob;
+        private readonly ICoffeeVideoStorage coffeeVideoStorage;
+        private readonly IFilePickerDialogService filePickerDialogService;
+        private readonly IMessageDialogService messageDialogService;
+        private readonly IMainViewModel mainViewModel;
+
 
         // 05/19/2021 05:43 am - SSN - [20210519-0529] - [004] - M03-05 - Show the blob URI of the uploaded blob
-        public CoffeeVideoViewModel(CloudBlockBlob cloudBlockBlob)
+        public CoffeeVideoViewModel(
+            CloudBlockBlob _cloudBlockBlob
+            //ICoffeeVideoStorage _coffeeVideoStorage,
+            //IFilePickerDialogService _filePickerDialogService,
+            //IMessageDialogService _messageDialogService,
+            //IMainViewModel _mainViewModel
+            )
         {
-            this.cloudBlockBlob = cloudBlockBlob;
+            this.cloudBlockBlob = _cloudBlockBlob;
+            // this.coffeeVideoStorage = _coffeeVideoStorage
+            //this.filePickerDialogService = _filePickerDialogService;
+            //this.messageDialogService = _messageDialogService;
+
+            //this.mainViewModel = _mainViewModel;
+            
         }
+
 
         //public string BlobName { get; set; }
         public string BlobName => cloudBlockBlob.Name;
@@ -19,6 +39,10 @@ namespace WiredBrainCoffee.AdminApp.ViewModel
         // public string BlobUri { get; set; }
         public string BlobUri => cloudBlockBlob.Uri.ToString();
 
+    }
+
+    public interface IMainViewModel
+    {
     }
 
 }
