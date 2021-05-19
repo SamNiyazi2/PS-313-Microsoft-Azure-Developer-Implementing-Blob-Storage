18e37fa0ed346ae7e23b9979d3cba4584b3cf5d9
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-05-19 00:36:06 -0500

    M03-03 - Completedl

--- WiredBrainCoffee.Storage/CoffeeVideoStorage.cs
+++ WiredBrainCoffee.Storage/CoffeeVideoStorage.cs
@@ -1,19 +1,53 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Threading.Tasks;
 
 namespace WiredBrainCoffee.Storage
 {
   public class CoffeeVideoStorage : ICoffeeVideoStorage
   {
-    public CoffeeVideoStorage()
+        private readonly string blobStorageConnectionString;
+        private readonly string blobContainerName = "CoffeeVideos";
+        public CoffeeVideoStorage(string blobStorageConnectionString)
     {
-    }
+            this.blobStorageConnectionString = blobStorageConnectionString;
+        }
 
     public async Task UploadVideoAsync(byte[] videoByteArray, string blobName)
     {
-      // TODO: Upload the video to Blob Storage
-    }
+            // 05/19/2021 12:06 am - SSN - [20210518-2359] - [001] - M03-03 - Upload a blob to a container 
 
-    public async Task<bool> CheckIfBlobExistsAsync(string blobName)
+            //string accountName = "";
+            //string keyValue = "";
+
+          
+            //var cloudStorageAccount_1 = new CloudStorageAccount(
+            //    new Microsoft.Azure.Storage.Auth.StorageCredentials( accountName, keyValue)
+            //    ,true);
+
+
+            if ( string.IsNullOrWhiteSpace(blobStorageConnectionString))
+            {
+                throw new Exception("Calling with null or empty storage connection string");
+            }
+
+            var cloudStorageAccount_2 = CloudStorageAccount.Parse(blobStorageConnectionString);
+
+            var cloudBlobClient = cloudStorageAccount_2.CreateCloudBlobClient();
+
+            var cloudBlobContainer = cloudBlobClient.GetContainerReference(blobContainerName);
+
+            await cloudBlobContainer.CreateIfNotExistsAsync(BlobContainerPublicAccessType.Blob, null, null);
+
+            var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(blobName);
+
+            await cloudBlockBlob.UploadFromByteArrayAsync(videoByteArray, 0, videoByteArray.Length);
+
+
+        }
+
+        public async Task<bool> CheckIfBlobExistsAsync(string blobName)
     {
       // TODO: Check if the blob exists in Blob Storage
       return false;
